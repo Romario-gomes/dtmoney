a5109c80efabeab7ec2a13eb7c705405092b67c2
Romario Alves <romariogn10@gmail.com>
2022-07-29 07:16:14 -0300

    Feat(Dashboard, Summary, Transactions Table): add Components and styles

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import entradaImg from "../../assets/entradas.svg";
+import saidasImg from "../../assets/saidas.svg";
+import totalImg from "../../assets/total.svg";
+
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entradaImg} alt="Entradas"/>
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={saidasImg} alt="Saídas"/>
+        </header>
+        <strong>-R$1000,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total"/>
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
